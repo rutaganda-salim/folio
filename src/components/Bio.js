e06389ae6d9d37983782b739de132cc2b62c3154
Salim Rutaganda <rutagandasalim@gmail.com>
2024-10-27 22:27:31 +0200

    fix: bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,10 +6,15 @@ const Bio = () => {
   return (
     <div className="bio mt-20 container mx-auto overflow-hidden">
       <p ref={bioRef}>
-        I am a MERN-stack Developer specialized in building responsive web
-        applications using Next.js, MongoDB, Express, React, and Node. It
-        includes examples of my work, demonstrating my ability to design and
-        implement scalable, efficient, and maintainable applications.
+        I am a highly skilled engineer specializing in blockchain, Web3, and
+        smart contract development. With a strong foundation in frontend
+        technologies, I create seamless and dynamic user interfaces that
+        integrate efficiently with decentralized applications (dApps). My
+        expertise also extends to backend development and data structures and
+        algorithms (DSA), giving me a full-stack perspective on building
+        scalable and robust applications. I am passionate about leveraging
+        blockchain technology to build secure and innovative solutions that push
+        the boundaries of Web3.
       </p>
     </div>
   );
